13500e0f4fd3e6cb0b601d1fa0295aad8ca937d7
suneetpathangay <suneetpathangay@gmail.com>
2024-01-16 00:49:49 -0500

    first commit

--- main.js
+++ main.js
@@ -3,31 +3,27 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 
-
 const scene=new THREE.Scene()
 
 const size={
-  width:window.innerWidth,
+  width:window.innerWidth/2,
   height:window.innerHeight
 }
 
 
-//takes in fielf of view and aspect ratio
 const camera=new THREE.PerspectiveCamera(45,size.width/size.height,0.1,100)
-//moving camera to the z axis to get that 3d view
+
 camera.position.z=40
+scene.background = new THREE.Color(0xffffff);
 scene.add(camera)
 
 
 const light=new THREE.PointLight(0xffffff)
 light.position.set(20,20,20)
 
-//light to see verything
+
 const ambientLight=new THREE.AmbientLight(0xffffff)
-
-
 scene.add(light,ambientLight)
-
 
 const canvas=document.querySelector('.webgl')
 
@@ -50,45 +46,15 @@ controls.autoRotateSpeed=1;
 window.addEventListener('resize',()=>{
   size.width=window.innerWidth,
   size.height=window.innerHeight
-  //update camera
+  
   camera.updateProjectionMatrix()
   camera.aspect=size.width/size.height
 
   renderer.setSize(size.width,size.height)
 })
 
-const addStar=()=>{
-  const geometry=new THREE.SphereGeometry(0.25,24,24)
-  const material=new THREE.MeshStandardMaterial({color:0xffffff})
-  const star=new THREE.Mesh(geometry,material)
-
-  const[x,y,z]=Array(3).fill().map(()=>THREE.MathUtils.randFloatSpread(100))
-  star.position.set(x,y,z)
-  scene.add(star)
-}
-
-Array(200).fill().forEach(addStar)
 
 
-
-
-//adding the sun
-
-// const sphere=new THREE.SphereGeometry(8,64,64)
-
-// const sunTexture=new THREE.TextureLoader().load('/public/images/sun.jpeg')
-
-// const sun=new THREE.Mesh(
-//     sphere,
-//   new THREE.MeshBasicMaterial({map:sunTexture,opacity:1,transparent:true}),
-
-// )
-// sun.position.set(-2,0,0)
-
-
-// scene.add(sun)
-
-//adding the torus
 
 const sphereGeo=new THREE.SphereGeometry(4,64,64)
 const sphereMaterial=new THREE.MeshBasicMaterial({color:0xffffff,wireframe:true})
@@ -107,28 +73,57 @@ const torus=new THREE.Mesh(
 scene.add(torus)
 
 
-
-
-// const geometry=new THREE.SphereGeometry(8,64,64)
-
-// const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } ); 
-// const sphere = new THREE.Mesh( geometry, material ); 
-
-// scene.add(sphere)
-
-
-
-
-
-
-
-
 const loop=()=>{
   controls.update()
   renderer.render(scene,camera)
   window.requestAnimationFrame(loop)
-  sun.rotation.y+=0.005;
+  
+  console.log(window.innerWidth+" "+window.innerWidth/2)
  
 }
-
 loop()
+
+
+//script functions
+
+const test=()=>{
+  window.open('https://github.com/suneepathangay')
+}
+
+const linkedin=()=>{
+  window.open('https://www.linkedin.com/in/suneet-pathangay-25137021a/')
+}
+
+const resume=()=>{
+  window.open('https://media.licdn.com/dms/document/media/D4E2DAQHCpP6a8MQNQA/profile-treasury-document-pdf-analyzed/0/1697933141863?e=1706140800&v=beta&t=v-TQgeKvn_YFEiG0nBscU-WQKJBI0hPQ07WYTvfpRuE')
+
+}
+
+const showPop=(event)=>{
+  const card = event.target;
+  const popup = card.querySelector('.popup');
+  popup.style.visibility = 'visible';
+  popup.style.display='block'
+}
+
+const closePop=(event)=> {
+  const card = event.target;
+  const popup = card.querySelector('.popup');
+  popup.style.visibility = 'hidden';
+}
+
+const openChess=()=>{
+  window.open('https://github.com/suneepathangay/Chess')
+}
+
+const openSum=()=>{
+  window.open('https://github.com/suneepathangay/Sum-News2')
+}
+
+window.test = test;
+window.linkedin=linkedin;
+window.resume=resume;
+window.showPop=showPop;
+window.closePop=closePop;
+window.openChess=openChess;
+window.openSum=openSum;
